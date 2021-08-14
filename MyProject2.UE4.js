d1098f2a16d88c798932aac35b9ec95b61abb93e
TheRocketDSGN <88950705+TheRocketDSGN@users.noreply.github.com>
2021-08-15 02:02:02 +0300

    Update MyProject2.UE4.js

--- MyProject2.UE4.js
+++ MyProject2.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject2.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/TheRocketDSGN/TestDriveUE/12b1ff73b5fd91478ad1aefe5d1b8e2ca815744c/MyProject2.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
