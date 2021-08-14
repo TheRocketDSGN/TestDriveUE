12b1ff73b5fd91478ad1aefe5d1b8e2ca815744c
TheRocketDSGN <88950705+TheRocketDSGN@users.noreply.github.com>
2021-08-15 01:58:45 +0300

    Update MyProject2.data.js

--- MyProject2.data.js
+++ MyProject2.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'E:/~Unreal Engine/Migrate/01/MyProject2/Binaries/HTML5/MyProject2.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject2.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/kg1s66w38mat3ru/MyProject2.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/kg1s66w38mat3ru/MyProject2.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
